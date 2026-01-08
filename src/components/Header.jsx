91396ef54f6484dbdb7ba8722e83131c1e60204c
gustavo2023 <gustavoegutierrezg23@gmail.com>
2026-01-08 18:22:21 -0400

    feat: add shadow to Header component for improved visual hierarchy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import ThemeToggler from "./ThemeToggler.jsx";
 
 function Header() {
   return (
-    <div className="flex justify-between bg-white dark:bg-blue-900 p-8 lg:px-12">
+    <div className="flex justify-between bg-white dark:bg-blue-900 p-8 lg:px-12 shadow-header">
       <h1 className="text-sm lg:text-2xl text-grey-950 dark:text-white font-extrabold">
         Where in the world?
       </h1>
