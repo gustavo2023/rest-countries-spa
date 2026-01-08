845bdcc05fac799def1d4e450a0bd14507b6bc53
gustavo2023 <gustavoegutierrezg23@gmail.com>
2026-01-08 18:12:15 -0400

    feat: add Header and ThemeToggler components for theme switching functionality

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import ThemeToggler from "./ThemeToggler.jsx";
+
+function Header() {
+  return (
+    <div className="flex justify-between bg-white dark:bg-blue-900 p-8 lg:px-12">
+      <h1 className="text-sm lg:text-2xl text-grey-950 dark:text-white font-extrabold">
+        Where in the world?
+      </h1>
+      <ThemeToggler />
+    </div>
+  );
+}
+
+export default Header;
