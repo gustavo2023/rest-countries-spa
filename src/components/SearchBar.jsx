2e9b8824acaa5323e18288b23f59d35eed5a3525
gustavo2023 <gustavoegutierrezg23@gmail.com>
2026-01-11 13:45:51 -0400

    feat: add SearchBar component and integrate it into HomePage for country search functionality

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,34 @@
+function SearchBar({ searchCountry, onSearch }) {
+  return (
+    <div className="flex items-center justify-start gap-6 px-8 py-4 bg-white dark:bg-blue-900 rounded-(--rounded-card) shadow-input">
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          viewBox="0 0 18 18"
+          fill="currentColor"
+          className="text-grey-300 dark:text-white"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M12.5 11H11.7L11.4 10.7C12.4 9.6 13 8.1 13 6.5C13 2.9 10.1 0 6.5 0C2.9 0 0 2.9 0 6.5C0 10.1 2.9 13 6.5 13C8.1 13 9.6 12.4 10.7 11.4L11 11.7V12.5L16 17.5L17.5 16L12.5 11ZM6.5 11C4 11 2 9 2 6.5C2 4 4 2 6.5 2C9 2 11 4 11 6.5C11 9 9 11 6.5 11Z"
+            fill="curentColor"
+            stroke="currentColor"
+          />
+        </svg>
+      </div>
+      <input
+        className="w-full text-grey-400 dark:text-white placeholder:text-grey-250 dark:placeholder:text-white focus:outline-none bg-transparent"
+        type="text"
+        placeholder="Search for a country..."
+        name="searchBar"
+        value={searchCountry}
+        onChange={(e) => onSearch(e.target.value)}
+      />
+    </div>
+  );
+}
+
+export default SearchBar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import useFetch from "../hooks/useFetch";
 import CountryCard from "../components/CountryCard";
+import SearchBar from "../components/SearchBar";
 
 function HomePage() {
+  const [searchCountry, setSearchCountry] = useState("");
+
   const {
     data: countries,
     loading,
@@ -10,14 +14,26 @@ function HomePage() {
     "https://restcountries.com/v3.1/all?fields=flags,name,population,region,capital"
   );
 
+  const filteredCountries = countries?.filter((country) =>
+    country.name.common
+      .toLowerCase()
+      .includes(searchCountry.toLowerCase())
+  );
+
   return (
-    <div className="p-8">
-      {loading && <p className="text-center mt-8">Loading...</p>}
-      {error && <p className="text-center mt-8 text-red-500">{error}</p>}
+    <div className="p-10">
+      <div className="mb-10">
+        <SearchBar searchCountry={searchCountry} onSearch={setSearchCountry} />
+      </div>
+
+      {loading && <p className="text-center text-2xl mt-8">Loading...</p>}
+      {error && (
+        <p className="text-center text-2xl mt-8 text-red-500">{error}</p>
+      )}
 
       {!loading && !error && countries && (
-        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 md:gap-14">
-          {countries.map((country) => (
+        <ul className="p-4 md:px-0 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 md:gap-14">
+          {filteredCountries.map((country) => (
             <li key={country.name.common}>
               <CountryCard country={country} />
             </li>
